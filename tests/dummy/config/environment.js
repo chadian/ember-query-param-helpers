f4f6eee34b0469c9fdf30791aeeb80734f2e3beb
Chad Carbert <chadcarbert@me.com>
2018-12-02 00:14:36 +0100

    Update dummy app rootURL for deployment on gh-pages

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -44,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/your-repo-name';
+    ENV.rootURL = 'ember-query-param-helpers';
     ENV.locationType = 'hash';
   }
 
